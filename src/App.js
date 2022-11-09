3ebe65ee32dbc094e6927e4c5f670053d13722d9
1seul357 <1seul357@gmail.com>
2022-11-09 17:51:05 +0900

    Style: FirstLevel 페이지 CSS 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import FirstLevel from './pages/FirstLevel';
 import './css/App.css';
+import '../style.css';
 
 export default class App {
   constructor($target) {
--- src/pages/FirstLevel.js
+++ src/pages/FirstLevel.js
@@ -1,14 +1,26 @@
+import '../css/FirstLevel.css';
+import Bear from "../assets/Bear.png";
+import Chick from "../assets/Chick.png";
+import Deer from "../assets/Deer.png";
+
 export default class FirstLevel {
     constructor({ $target }) {
       this.section = document.createElement("section");
       $target.appendChild(this.section);
+      this.div = document.createElement("section");
+      this.div.className = "box";
       this.render();
     }
   
     render() {
-      this.section.innerHTML = "";
-      const text = document.createElement("h1");
-      text.innerText = "첫 번째 페이지";
-      this.section.appendChild(text);
+      this.div.innerHTML = "";
+      const text = document.createElement("h3");
+      text.className = "mainText"
+      text.innerText = "한 글자인 동물을 모두 선택해주세요.";
+      this.div.appendChild(text);
+      const imgTag = document.createElement('img');
+      imgTag.src = Chick;
+      this.section.appendChild(this.div);
+      this.div.appendChild(imgTag);
     }
   }
