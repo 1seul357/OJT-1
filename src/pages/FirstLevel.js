5ce896589c3255a350f85d9d9733f27d3e12a17a
1seul357 <1seul357@gmail.com>
2022-11-09 23:24:34 +0900

    Refactor: 코드 리팩터링

--- src/pages/FirstLevel.js
+++ src/pages/FirstLevel.js
@@ -2,25 +2,32 @@ import '../css/FirstLevel.css';
 import Bear from "../assets/Bear.png";
 import Chick from "../assets/Chick.png";
 import Deer from "../assets/Deer.png";
+import Elephant from "../assets/Elephant.png"
+import Fox from "../assets/Fox.png"
+import Koala from "../assets/Koala.png"
+import Peng from "../assets/Peng.png"
+import Sheep from "../assets/Sheep.png"
+
 
 export default class FirstLevel {
     constructor({ $target }) {
       this.section = document.createElement("section");
+      this.section.className = "firstSection";
       $target.appendChild(this.section);
-      this.div = document.createElement("section");
-      this.div.className = "box";
       this.render();
     }
   
     render() {
-      this.div.innerHTML = "";
+      this.section.innerHTML = "";
+
       const text = document.createElement("h3");
       text.className = "mainText"
       text.innerText = "한 글자인 동물을 모두 선택해주세요.";
-      this.div.appendChild(text);
+      this.section.appendChild(text);
+
       const imgTag = document.createElement('img');
       imgTag.src = Chick;
-      this.section.appendChild(this.div);
-      this.div.appendChild(imgTag);
+
+      this.section.appendChild(imgTag);
     }
   }
