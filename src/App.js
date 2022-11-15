46de63c9e3e13d995ad8bb6f5c826ad05c877513
1seul357 <1seul357@gmail.com>
2022-11-15 15:14:27 +0900

    Feat: 프로필 선택 페이지 JS, CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import Index from "./pages/Index";
 import FirstLevel from "./pages/FirstLevel";
 import SecondLevel from "./pages/SecondLevel";
 import ThirdLevel from "./pages/ThirdLevel";
 import Success from "./pages/Success";
+import LocalStorage from "./util/LocalStorage";
+import Profile1 from "./assets/Profile1.png";
+import Profile2 from "./assets/Profile2.png";
+import Profile3 from "./assets/Profile3.png";
+import Profile4 from "./assets/Profile4.png";
+import Profile5 from "./assets/Profile5.png";
 import Bear from "./assets/Bear.png";
 import Chick from "./assets/Chick.png";
 import Deer from "./assets/Deer.png";
@@ -23,21 +30,26 @@ import "./css/App.css";
 import "../style.css";
 
 export default function App($target) {
-  let level = 3;
+  let level = 0;
   const start = new Date();
 
   const loadPage = () => {
     $target.innerHTML = "";
+    const profile = JSON.parse(LocalStorage.getItem("profile"));
+    if (level === 0) {
+      const data = { img: [ Profile1, Profile2, Profile3, Profile4, Profile5 ], name: { "정우": false, "민준": false, "예지": false, "우찬": false, "슬기": false }, message: "함께 학습을 진행할 친구를 골라주세요." };
+      new Index({ $target, loadPage, data});
+    }
     if(level === 1) {
-      const data = { animal:[Peng, Koala, Bear, Deer, Elephant, Fox, Sheep, Chick], problemAnswer: [2, 3, 6], text: "한 글자", message: "인 동물을 모두 선택해주세요." };
+      const data = { profile, animal:[Peng, Koala, Bear, Deer, Elephant, Fox, Sheep, Chick], problemAnswer: [2, 3, 6], text: "한 글자", message: "인 동물을 모두 선택해주세요." };
       new FirstLevel({ $target, loadPage, data });
     };
     if (level == 2) {
-      const data = { text: "7개 이상", message: "의 네모 칸을 색칠해주세요." };
+      const data = { profile, text: "7개 이상", message: "의 네모 칸을 색칠해주세요." };
       new SecondLevel({ $target, loadPage, data });
     }
     if (level === 3) {
-      const data = { circle: [Red, Pink, Yellow, SkyBlue, Blue, Green, Grey, Orange, DarkGreen,], text: "3개", message: "의 원을 드래그해서 박스에 담아주세요." };
+      const data = { profile, circle: [Red, Pink, Yellow, SkyBlue, Blue, Green, Grey, Orange, DarkGreen,], text: "3개", message: "의 원을 드래그해서 박스에 담아주세요." };
       new ThirdLevel({ $target, loadPage, data });
     }
     if (level === 4) {
